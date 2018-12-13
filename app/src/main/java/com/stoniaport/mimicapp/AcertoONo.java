e12431e005a37b232995d06b4fb241f6c0dc1e0d
rodriguezsebastian <rodriguezsebastian@conicet.gov.ar>
2018-12-13 17:24:54 -0300

    Se Agrego un Timmer de 1 minuto y se agrego una pantalla donde pregunta si se acerto la pelicula o no

---
+++ app/src/main/java/com/stoniaport/mimicapp/AcertoONo.java
@@ -0,0 +1,25 @@
+package com.stoniaport.mimicapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AcertoONo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerto_ono);
+    }
+
+    public void Si(View v) {
+        Intent acerto = new Intent(AcertoONo.this, MainActivity.class);
+        startActivity(acerto);
+    }
+
+    public void No(View v) {
+        Intent noAcerto = new Intent(AcertoONo.this, MainActivity.class);
+        startActivity(noAcerto);
+    }
+}
--- app/src/main/java/com/stoniaport/mimicapp/MainActivity.java
+++ app/src/main/java/com/stoniaport/mimicapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.stoniaport.mimicapp;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,63 +11,124 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button siguiente;
-    int contador;
-    int numero=1;
+    int puntos;
+    int numero = 1;
 
-    Equipo equipo1 = new Equipo("Equipo 1",1,0);
-    Equipo equipo2 = new Equipo("Equipo 2",2,0);
+    Equipo equipo1 = new Equipo("Equipo 1", 1, 0);
+    Equipo equipo2 = new Equipo("Equipo 2", 2, 0);
     Equipo equipoActual = equipo1;
+
+    private CountDownTimer countDownTimer;
+    private TextView countDownText;
+    private Button countDownButton;
+    private long timeLeftInMiliseconds = 60000; // 1min
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        contador=0;
+
+        countDownText = findViewById(R.id.tiempo);
+        countDownButton = findViewById(R.id.buttonTime);
+
+        puntos = 0;
         mostrarResultado();
+
+        countDownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
+        });
 
     }
 
     public void pelicula(View Vista) {
-                Intent siguiente = new Intent(MainActivity.this, pelicula.class);
-                startActivity(siguiente);
+        Intent siguiente = new Intent(MainActivity.this, pelicula.class);
+        startActivity(siguiente);
     }
 
-    public void incrementaContador(View Vista){
-        contador++;
+    public void incrementaContador(View Vista) {
+        puntos = (equipoActual.getPuntos()) + 1;
+        equipoActual.setPuntos(puntos);
         mostrarResultado();
     }
 
-    public void decrementaContador(View Vista){
-        contador--;
+    public void decrementaContador(View Vista) {
+        puntos = (equipoActual.getPuntos()) - 1;
+        equipoActual.setPuntos(puntos);
         mostrarResultado();
     }
 
-    public void reseteaContador(View Vista){
-        contador=0;
+    public void reseteaContador(View Vista) {
+        equipoActual.setPuntos(0);
         mostrarResultado();
     }
 
-    public void mostrarResultado(){
 
-
-        TextView text = (TextView)findViewById(R.id.puntos);
-        text.setText("" + contador);
+    public void mostrarResultado() {
+        TextView text = findViewById(R.id.puntos);
+        puntos = equipoActual.getPuntos();
+        String puntosS = Integer.toString(puntos);
+        text.setText(puntosS);
     }
 
 
+    public void cambioDeEquipo(View Vista) {
 
-    public void cambioDeEquipo(View Vista){
-
-        TextView text = (TextView)findViewById(R.id.nombre);
+        TextView text = findViewById(R.id.nombre);
         numero++;
-        if(numero==2 ){
+        if (numero == 2) {
             equipoActual = equipo2;
-        }
-        else {
-            numero=1;
-            equipoActual=equipo1;
+        } else {
+            numero = 1;
+            equipoActual = equipo1;
         }
         text.setText(equipoActual.getNombre());
+        mostrarResultado();
     }
+
+
+
+    //--------------------------------Timer----------------------------------------------
+
+
+    public void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeftInMiliseconds, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMiliseconds = millisUntilFinished;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+                countDownText.setText("1:00");
+                timeLeftInMiliseconds= 60000;
+                Intent AcertoONo = new Intent(MainActivity.this, AcertoONo.class);
+                startActivity(AcertoONo);
+            }
+        }.start();
+    }
+
+
+    public void updateTimer(){
+        int minutes = (int) timeLeftInMiliseconds / 60000;
+        int seconds = (int) timeLeftInMiliseconds % 60000/1000;
+
+        String timeLeftText;
+
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+        if(seconds<10) timeLeftText +="0";
+        timeLeftText += seconds;
+
+        countDownText.setText(timeLeftText);
+    }
+
+    //--------------------------------------------------------------------------------------
+
 }
+
