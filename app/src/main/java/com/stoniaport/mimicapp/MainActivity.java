ca32843e330afb4ad7e972ee5ac8d4a2e23d2932
rodriguezsebastian <rodriguezsebastian@conicet.gov.ar>
2018-12-13 14:57:16 -0300

    agrega un contador y modifica la forma de llamar a la otra pantalla

--- app/src/main/java/com/stoniaport/mimicapp/MainActivity.java
+++ app/src/main/java/com/stoniaport/mimicapp/MainActivity.java
@@ -5,26 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button siguiente;
+    int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contador=0;
 
+    }
 
-        siguiente=(Button) findViewById(R.id.pelicula);
-
-        siguiente.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
+    public void pelicula(View Vista) {
                 Intent siguiente = new Intent(MainActivity.this, pelicula.class);
                 startActivity(siguiente);
-            }
-        });
     }
+
+    public void incrementaContador(View Vista){
+        contador++;
+        mostrarResultado();
+    }
+
+    public void decrementaContador(View Vista){
+        contador--;
+        mostrarResultado();
+    }
+
+    public void reseteaContador(View Vista){
+        contador=0;
+        mostrarResultado();
+    }
+
+    public void mostrarResultado(){
+
+        TextView text = (TextView)findViewById(R.id.puntos);
+        text.setText("" + contador);
+    }
+
 }
