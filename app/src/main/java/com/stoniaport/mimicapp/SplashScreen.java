cdd365b916eb60dab1c9d5be28b603db47140fe1
rodriguezsebastian <rodriguezsebastian@conicet.gov.ar>
2018-12-26 17:44:58 -0300

    Cambios visuales, agregado de style button, agrega un splash view(antes de comenzar la app una ventana de muestra)

---
+++ app/src/main/java/com/stoniaport/mimicapp/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.stoniaport.mimicapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+    }
+
+}
